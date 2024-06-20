aaf89f37f130ac02d87eba56492a2ace7d9749fd
priya926 <anubhutisingh926@gmail.com>
2024-06-20 22:39:05 +0530

    pushing env file and database
    pushing env file and database

---
+++ restaurant (2).sql
@@ -0,0 +1,440 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2022 at 03:57 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+(1, 'Admin', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `subject` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`, `created_at`, `updated_at`) VALUES
+(1, 'Test User', 'testuser3@gmail.com', 'kfjdif', 'nfjrf', NULL, NULL),
+(2, 'Test User3', 'testuser3@gmail.com', 'demoo3', 'dddd', NULL, NULL),
+(3, 'demo', 'demo@gmail.com', 'demoo3', 'nfjrf', NULL, NULL),
+(4, 'Dhairya', 'dhairya@gmail.com', 'finally testing', 'Final test msg', NULL, NULL),
+(5, 'TestUser', 'testuser@gmail.com', 'Testing', 'testing message', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `helps`
+--
+
+CREATE TABLE `helps` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `question` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `helps`
+--
+
+INSERT INTO `helps` (`id`, `question`, `created_at`, `updated_at`) VALUES
+(1, 'ljhhhhhh', NULL, NULL),
+(2, 'hii i am having issue', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_08_02_060225_create', 1),
+(6, '2022_08_03_044018_contacts', 1),
+(7, '2022_08_03_101816_uregistration', 1),
+(8, '2022_08_03_102055_ufeedback', 1),
+(9, '2022_08_03_102114_ubooking', 1),
+(10, '2022_08_03_102212_restroreg', 1),
+(11, '2022_08_03_102238_restrodetail', 1),
+(12, '2022_08_03_102318_restrofacility', 1),
+(13, '2022_08_03_102340_restroimage', 1),
+(14, '2022_08_03_102403_adminlogin', 1),
+(15, '2022_08_03_102729_adminrfacility', 1),
+(16, '2022_08_03_103020_adminimages', 1),
+(17, '2022_08_03_103043_restrohelp', 1),
+(18, '2022_08_03_103100_userhelp', 1),
+(19, '2022_08_03_101816_uregistrations', 2),
+(20, '2022_08_12_060057_tables', 2),
+(21, '2022_08_12_074734_uregistrations', 3),
+(22, '2022_08_12_082517_rregistrations', 4),
+(23, '2022_08_13_055557_rdetails', 5),
+(24, '2022_08_13_085231_admins', 5),
+(25, '2022_08_13_110056_admins', 6),
+(26, '2022_08_16_070650_create_restrodetails_table', 7),
+(27, '2022_08_17_054815_tables', 8),
+(28, '2022_08_17_170001_helps', 9),
+(29, '2022_08_21_080214_users', 10),
+(30, '2022_08_21_101557_restrodetails', 11),
+(31, '2022_08_21_112248_restrodetails', 12),
+(32, '2022_08_21_161702_tables', 13),
+(33, '2022_08_22_014648_tables', 14),
+(34, '2022_08_22_015206_tables', 15),
+(35, '2022_08_22_015456_tables', 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restrodetails`
+--
+
+CREATE TABLE `restrodetails` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `restro_id` bigint(20) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cost_per_two` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cuisine` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `otime` time NOT NULL,
+  `ctime` time NOT NULL,
+  `wotime` time NOT NULL,
+  `wctime` time NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `must_try` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `select` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `offer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `restro_facilities` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `iep` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `restrodetails`
+--
+
+INSERT INTO `restrodetails` (`id`, `restro_id`, `name`, `contact`, `email`, `address`, `cost_per_two`, `cuisine`, `otime`, `ctime`, `wotime`, `wctime`, `description`, `must_try`, `select`, `offer`, `restro_facilities`, `tp`, `iep`, `mp`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Four Seasons Restaurant', '9929602533', 'fourseasons@gmail.com', 'Udaipur,Rajasthan', '1000', 'Indian', '09:00:00', '22:00:00', '09:00:00', '23:30:00', 'Four Seasons brings authentic Indian cuisine in a setting that is unabashedly Indian. Our chefs have endeavored to stay true to the actual origin of each regional dish, honestly recreating flavors that one would normally associate only with home cooked food of that region.Part art, part imaginative, the interiors of Four Seasons have been put together with India\'s finery, frivolity and sheer joie de vivre in mind. From furniture to fabrics, pearls to periodicals, carpets to cutlery... with antique tables as counters for coal irons, colorful street food vendors, grandfather clocks jostling with stereos, bangle sellers and old jewelry sharing space with auto parts, it captures the spirit of India in all its eclectic irreverence. In our restaurant, \"nothing matches\" yet \"everything gels\".', 'Paneer-Tikka', '3', 'Mon-Wed-Fri 30% Discount', '\"[\\\"Air Conditioned\\\",\\\"Wifi\\\",\\\"Parking\\\"]\"', '1661081578.jpg', '1661081578.jpg', '1661081578.jpg', '2022-08-21 06:02:58', '2022-08-21 06:02:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tables`
+--
+
+CREATE TABLE `tables` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `person` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`id`, `name`, `email`, `contact`, `date`, `time`, `person`, `created_at`, `updated_at`) VALUES
+(1, 'Dhairya', 'dhairya@gmail.com', '9976543210', '2022-08-23', '13:43:00', '2', '2022-08-22 00:44:12', '2022-08-22 00:44:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uregistrations`
+--
+
+CREATE TABLE `uregistrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `uregistrations`
+--
+
+INSERT INTO `uregistrations` (`id`, `name`, `email`, `contact`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Dhairya', 'dhairya@gmail.com', '9876543210', '$2y$10$XHNkFDkYw.tL3HrnIweBremgUx1qRFtJgeJYCV0Ihcg95f3OMWd5O', '2022-09-25 22:12:06', '2022-09-25 22:12:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `contact`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Four Seasons Restaurant', 'fourseasons@gmail.com', '9929602533', '$2y$10$tfk5kbQOx5xbIGUzbo4vFeUfRK4Emae52lG2zGHzBbT466m0GdLpq', '2022-08-21 02:42:34', '2022-08-21 02:42:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `helps`
+--
+ALTER TABLE `helps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `restrodetails`
+--
+ALTER TABLE `restrodetails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `uregistrations`
+--
+ALTER TABLE `uregistrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `helps`
+--
+ALTER TABLE `helps`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `restrodetails`
+--
+ALTER TABLE `restrodetails`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tables`
+--
+ALTER TABLE `tables`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `uregistrations`
+--
+ALTER TABLE `uregistrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
